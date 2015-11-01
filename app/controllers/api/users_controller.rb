d9db9b1008c3b368e200a37e22d8aa95b988ae71
James Carney <jmcarneyiii@gmail.com>
2015-11-01 18:22:37 -0500

    Build users controller with create & show methods

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,27 @@
+class Api::UsersController < ActionController::Base
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if user
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
